a5bfba52645ca0d71fe1457219c9c4b9df0b47cf
Amarjit kumar <M1044368@mindtree.com>
2018-08-31 21:59:06 +0530

    first commit

---
+++ ADO_NET_DATASET/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ADO_NET_DATASET
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["InventoryConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter("spGetDataFromTwoTable", con);
+                adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+
+                GridView1.DataSource = dataSet.Tables[0];
+                GridView1.DataBind();
+
+                GridView2.DataSource = dataSet.Tables[1];
+                GridView2.DataBind();
+            }
+        }
+    }
+}
